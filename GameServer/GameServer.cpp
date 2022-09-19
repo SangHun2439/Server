f62a17ae1579609693878ca1d12d198e589f047c
Brad <privacyab@gmail.com>
2022-09-20 03:37:36 +0900

    IocpCore 추가

--- GameServer/GameServer.cpp
+++ GameServer/GameServer.cpp
@@ -14,41 +14,24 @@
 #include <windows.h>
 #include <future>
 
+#include "SocketUtils.h"
+#include "Listener.h"
+#include "IocpCore.h"
+
 int main()
 {
-	WSAData wsaData;
-	if (::WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		return 0;
+	Listener listener;
+	listener.StartAccept(NetAddress(L"127.0.0.1", 7777));
 
-	SOCKET listenSocket = ::socket(AF_INET, SOCK_STREAM, 0);
-	if (listenSocket == INVALID_SOCKET)
+	for (int32 i = 0; i < 5; ++i)
 	{
-		int32 errCode = ::WSAGetLastError();
-		cout << "Socket ErrCode : " << errCode << endl;
-		return 0;
+		GThreadManager->Launch([=]()
+			{
+				while (true)
+				{
+					GIocpCore.Dispatch();
+				}
+			});
 	}
-
-	u_long on;
-	if (::ioctlsocket(listenSocket, FIONBIO, &on) == INVALID_SOCKET)
-		return 0;
-
-	SOCKADDR_IN serverAddr;
-	::memset(&serverAddr, 0, sizeof(serverAddr));
-	serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = ::htonl(INADDR_ANY);
-	serverAddr.sin_port = ::htons(7777);
-
-	if (::bind(listenSocket, (SOCKADDR*)&serverAddr, sizeof(serverAddr)) == SOCKET_ERROR)
-		return 0;
-
-	if (::listen(listenSocket, SOMAXCONN) == SOCKET_ERROR)
-		return 0;
-
-	cout << "Accept" << endl;
-
-	// Select 모델 = (select 함수가 핵심이 되는)
-	
-
-	// 윈속 종료
-	::WSACleanup();
+	GThreadManager->Join();
 }
